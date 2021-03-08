1cc1de60243db165e3e7b6427c5835e9ce4dbae9
github-action <andnpatterson@gmail.com>
2021-03-08 17:27:58 +0000

    updating to version 0.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         "matplotlib>=3.3.0",
         "PyExpUtils @ git+ssh://git@github.com/andnp/PyExpUtils@2.4",
     ],
-    version=0.5,
+    version=0.6,
     license='MIT',
     description='A few plotting utilities to go with PyExpUtils',
     long_description='todo',
