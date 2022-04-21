6fb2a32af88de9e5d036b39732d17b576ab1ba65
andy <andnpatterson@gmail.com>
2022-04-20 22:32:36 -0600

    ci: get build tools setup

---
+++ scripts/publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+MYPYPATH=./typings mypy --ignore-missing-imports -p PyExpUtils
+
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
+git config user.email "andnpatterson@gmail.com"
+git config user.name "github-action"
+
+git fetch --all --tags
+
+git checkout -f main
+
+# sync package dependencies
+pipenv-setup sync --dev --pipfile
+
+# bump the version
+cz bump --no-verify --yes --check-consistency
+
+# push to pypi repository
+python -m build
+
+python -m twine upload -u __token__ -p ${PYPI_TOKEN} --non-interactive dist/*
+
+git push
+git push --tags
--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
-    name='PyExpPlotting',
+    name='PyExpPlotting-andnp',
     url='https://github.com/andnp/PyExpPlotting.git',
     author='Andy Patterson',
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*']),
+    package_data={"PyExpPlotting": ["py.typed"]},
+    version='0.8.0',
+    license='MIT',
+    description='A few plotting utilities to go with PyExpUtils',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
     install_requires=[
         "scipy>=1.5.4",
         "matplotlib>=3.3.0",
         "PyExpUtils>=2.4",
     ],
-    version=0.8,
-    license='MIT',
-    description='A few plotting utilities to go with PyExpUtils',
-    long_description='todo',
+    extras_require={},
 )
